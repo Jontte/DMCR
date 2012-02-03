f435aa4de7c9ff2e0234475979bfe807e66a8e87
Mikko Perttunen <cyndis@kapsi.fi>
2012-02-03 16:48:03 +0200

    Add beginnings of dmcr::Socket

---
+++ backend/src/socket.cpp
@@ -0,0 +1,95 @@
+#include "socket.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include "dmcr_protocol.pb.h"
+#include <cstdio>
+
+dmcr::Socket::Socket(const std::string &hostname, in_port_t port)
+    : m_hostname(hostname), m_port(port), m_listener(NULL),
+      m_fd(0), m_seq(0)
+{
+#ifdef DMCR_THREADED
+    pthread_spin_init(&m_spinlock, 0);
+#endif
+}
+
+dmcr::Socket::~Socket()
+{
+    close(m_fd);
+}
+
+void dmcr::Socket::setListener(IBackendSocketListener *listener)
+{
+    m_listener = listener;
+}
+
+void dmcr::Socket::connect()
+{
+    // Resolve host
+    struct addrinfo *addr;
+    struct addrinfo hints;
+    hints.ai_flags = AI_ADDRCONFIG;
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
+    hints.ai_canonname = NULL; hints.ai_addr = NULL; hints.ai_next = NULL;
+
+    char port_str[10];
+    snprintf(port_str, 10, "%d", m_port);
+
+    int r = getaddrinfo(m_hostname.c_str(), port_str, &hints, &addr);
+    if (r != 0)
+        throw SocketException("Could not resolve hostname");
+
+    int fd = 0;
+    for (; addr != NULL; addr = addr->ai_next) {
+        fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+        if (!fd)
+            continue;
+
+        if (::connect(fd, addr->ai_addr, addr->ai_addrlen) == -1) {
+            close(fd);
+            continue;
+        }
+
+        break;
+    }
+
+    if (addr == NULL) {
+        freeaddrinfo(addr);
+        throw SocketException("Could not connect to host");
+    }
+
+    freeaddrinfo(addr);
+
+    m_fd = fd;
+
+    sendHandshakePacket();
+}
+
+void dmcr::Socket::sendHandshakePacket()
+{
+    DmcrBackendHandshake packet;
+    packet.set_protocol_version(0);
+    packet.set_description("DMCR/1.0");
+    sendPacket(Packet_BackendHandshake, packet);
+}
+
+void dmcr::Socket::sendPacket(PacketId id,
+                              const ::google::protobuf::Message &msg)
+{
+#ifdef DMCR_THREADED
+    pthread_spin_lock(m_spinlock);
+#endif
+    DmcrPacketHeader header;
+    header.set_length(msg.ByteSize());
+    header.set_id((uint8_t)id);
+    header.set_seq(m_seq++);
+
+    header.SerializeToFileDescriptor(m_fd);
+    msg.SerializeToFileDescriptor(m_fd);
+#ifdef DMCR_THREADED
+    pthread_spin_unlock(m_spinlock);
+#endif
+}
---
+++ backend/src/socket.h
@@ -0,0 +1,69 @@
+#ifndef DMCR_SOCKET_H
+#define DMCR_SOCKET_H
+
+#include <cstdint>
+#include <string>
+#include <netinet/in.h>
+#include <google/protobuf/message.h>
+#ifdef DMCR_THREADED
+#include <pthread.h>
+#endif
+
+namespace dmcr {
+
+class SocketException {
+public:
+    SocketException(const std::string& m) :
+        message(m)
+    {}
+
+    const std::string message;
+};
+
+enum ConnectionResult { ConnectionResult_Success, ConnectionResult_InvalidKey,
+                        ConnectionResult_ConnectionFailed };
+
+enum PacketId { Packet_BackendHandshake = 1 };
+
+class Socket;
+
+class IBackendSocketListener {
+public:
+    virtual void onConnectionResult(Socket *socket,
+                                    ConnectionResult result,
+                                    const std::string& message) = 0;
+    virtual void onNewTask(Socket *socket, uint16_t width, uint16_t height,
+                           uint32_t iterations, const std::string& scene) = 0;
+
+};
+
+/* dmcr::Socket
+   Abstracted TCP-based communication method between the backend and controller.
+   Writing packets is NOT thread-safe at the moment!
+*/
+class Socket {
+public:
+    Socket(const std::string& hostname, in_port_t port);
+    ~Socket();
+
+    void setListener(IBackendSocketListener *listener);
+    void connect();
+
+private:
+    std::string m_hostname;
+    in_port_t m_port;
+    IBackendSocketListener *m_listener;
+#ifdef DMCR_THREADED
+    pthread_spinlock_t m_spinlock;
+#endif
+
+    int m_fd;
+    uint32_t m_seq;
+
+    void sendHandshakePacket();
+    void sendPacket(PacketId, const ::google::protobuf::Message &);
+};
+
+}
+
+#endif
