d0f06a6c16bd5ec4a9f57e3f502f1852ba268be4
Mikko Perttunen <cyndis@kapsi.fi>
2012-02-03 16:48:14 +0200

    Add socket testing code to main.cpp

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include "vector.h"
+#include "socket.h"
 
 // Bring some common standard library classes to scope cos we're lazy and want to type less
 using std::string;
@@ -15,6 +16,17 @@ int main(int argc, char * argv[])
     vector<string> args(argv, argv+argc);
 
     cout << "Hello, World!" << endl;
+
+    if (args.size() > 1) {
+        dmcr::Socket s(args[1], 9393);
+
+        try {
+            s.connect();
+        } catch (const dmcr::SocketException& e) {
+            cout << "SocketException: " << e.message << endl;
+        }
+    }
+
     return 0;
 }
 
