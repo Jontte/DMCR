8c7f6e530f4af1c2f0acf7206c1e7babe91aa176
Tatu Leinonen <tatu_leinonen@hotmail.com>
2012-03-11 18:48:21 +0200

    Modified camera.cpp with some pseudocode for ray calculations. Mathematical solution to issue #10

--- backend/src/camera.cpp
+++ backend/src/camera.cpp
@@ -2,10 +2,19 @@
 
 dmcr::Ray dmcr::Camera::ray(float /* x */, float /* y */) const
 {
-    /* 
+    /*
      * TODO
      * Implement me properly! (see issue #10)
+     *
+     *Pseudocode here:
+     at = normalize(self.look_at)
+     up = vector3(???)
+     horizontal = normalize(cross(at,up))
+     vertical 	= cross(horizontal,at)
+     H = tan(0.5 * FOV)
+     horizontal = H * horizontal
+     v = (1 / ASPECT) * H * vertical
+     return Ray(position, (2x-1)*horizontal + (2y-1)*vertical - position)
      */
     return Ray(m_position, m_look_at - m_position);
 }
-
