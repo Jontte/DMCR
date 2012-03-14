ee62050f4d83275828dfee58a6dfbcebbb41167e
Tatu Leinonen <tatu_leinonen@hotmail.com>
2012-03-14 18:27:50 +0200

    Implemented the Camera class, further advancing issue #10

--- backend/src/camera.cpp
+++ backend/src/camera.cpp
@@ -1,6 +1,6 @@
 #include "camera.h"
 
-dmcr::Ray dmcr::Camera::ray(float /* x */, float /* y */) const
+dmcr::Ray dmcr::Camera::ray(float x, float y) const
 {
     /*
      * TODO
@@ -10,11 +10,18 @@ dmcr::Ray dmcr::Camera::ray(float /* x */, float /* y */) const
      at = normalize(self.look_at)
      up = vector3(???)
      horizontal = normalize(cross(at,up))
-     vertical 	= cross(horizontal,at)
+     vertical       = cross(horizontal,at)
      H = tan(0.5 * FOV)
      horizontal = H * horizontal
-     v = (1 / ASPECT) * H * vertical
-     return Ray(position, (2x-1)*horizontal + (2y-1)*vertical - position)
+     vertical = (1 / ASPECT) * H * vertical
      */
-    return Ray(m_position, m_look_at - m_position);
+    at = (m_look_at - m_position).normalized();
+    up = dmcr::Vector3f(0,1,0);
+    horizontal = at.cross(up).normalized();
+    vertical = horizontal.cross(at);
+    H = tan(0.5 * m_fov * 180/ pi);
+    horizontal = H * horizontal;
+    vertical = (1 / m_aspect) * H * vertical;
+    return Ray(m_position, at + (2*x-1)*horizontal + (2*y-1)*vertical);
+    //return Ray(m_position, m_look_at - m_position); vanha
 }
