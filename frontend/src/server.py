c8eaa893a0e1d0edd3482c74a8bb03936e61dff1
Arttu <arttu.voutilainen@gmail.com>
2012-03-15 17:05:56 +0200

    Added docstrings to server.py

--- frontend/src/server.py
+++ frontend/src/server.py
@@ -14,22 +14,45 @@ import connection
 
 class Server(object):
     '''
-    classdocs
+    Server accepts connections and gives them tasks to do.
+    Use Server.FileToTask(filename) to set the scene to be rendered.
+    Then call Server.Listen() to begin listening.
+    
+    
     '''
 
-    def __init__(self):
+    def __init__(self, port = 9393):
         '''
-        Constructor
+        Constructor initializes server. Use Listen() to start accepting connections.
+        
+        
+        @param port: port to listen to (default 9393) 
+        
         '''
         self.host = ''
-        self.port = 9393
+        self.port = port
         self.connections = list()
     
     def FileToTask(self, filename):
+        '''
+        Reads a file and stores in self.task, which is given to every connection.
+        
+        @param filename: the filename of the json-file describing the scene.
+        
+        ''' 
         with open(filename,'r') as f:
             self.scene = '\n'.join(f.readlines())     
     
     def Listen(self):
+        '''
+        Starts the server. Server accepts connections and creates connection.Connections take care of each backend.
+        For each new backend self.scene is given to be rendered.
+          
+        Only way to stop nicely is KeyboardInterrupt. 
+        
+        @return: nothing
+        
+        '''
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
