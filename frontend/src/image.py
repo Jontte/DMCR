3642a0c5ebf646e45a7e8ae23fbfc301221dce57
Arttu <arttu.voutilainen@gmail.com>
2012-03-17 16:00:48 +0200

    Fixes everything in image.py.

--- frontend/src/image.py
+++ frontend/src/image.py
@@ -1,3 +1,6 @@
+#we want this cooler division here
+from __future__ import division
+
 '''
  This file is a part of the DMCR project and is subject to the terms and
  conditions defined in file 'LICENSE.txt', which is a part of this source
@@ -10,12 +13,12 @@ Created on Mar 17, 2012
 @author: blizzara
 '''
 
+
 #Requires PyPNG
 import assets.png as png
 
-#we want this cooler division here
-from __future__ import division
 
+# the exception to throw when something bad happens here
 class InvalidImage(Exception):
     def __init__(self, value):
         self.value = value
@@ -24,46 +27,82 @@ class InvalidImage(Exception):
 
 class Image(object):
     '''
-    classdocs
+    Image-class takes care of turning incoming binary data into images. 
+    Averages multiple pass images to one and writes png-files.
+    
+    
     '''
 
 
     def __init__(self, width, height):
         '''
-        Constructor
+        Initializes Image-instance with given width and height.
+        
+        @param width: image width in pixels
+        @param height: image height in pixels  
+        
         '''
-        self.iterations = 0
-        self.width = width
+        self.iterations = 0 # iterations initially 0
+        
+        self.width = width  # store image width and height
         self.height = height
-        self.image = [[[]*3]*height]*width
+        
+        # initialize boxed-row flat-pixel -list for storing image in memory, see pypng for details
+        self.image = [[0 for i in range(width*3)] for j in range(height)]
+        
+        # initialize png.Writer, see pypng for details 
         self.writer = png.Writer(width = self.width, height = self.height, bitdepth = 8)
     
     def AddFromString(self, data, iterations):
         '''
+        Average new image and the instance image. 
+        Actually sums the iteration-weighted values of new image into existing one.
+        Raises InvalidImage if data's images dimensions don't match instances.
         
         @param data: png as string
         @param iterations: how many iterations the new image contains
+        
+        @return: nothing
           
         '''
+        # enum return tuple of png.Reader()
         WIDTH = 0
         HEIGHT = 1
         PIXELS = 2
         META = 3
         
-        new_img = png.Reader(data=data).read() # (width, height, pixels, meta)
+        # read incoming image data into boxed-row flat-pixel list
+        new_img = png.Reader(bytes=data).read() # (width, height, pixels, meta)
+        
+        #test that image dimensions match
         if new_img[WIDTH] != self.width or new_img[HEIGHT] != self.height:
-            raise InvalidImage
-        
-        for w in xrange(self.width):
-            for h in xrange(self.height):
-                for i in range(3):
-                    self.image[w][h][i] += new_img[PIXELS][w][h][i]
-        
-        self.iterations += iterations
-        
+            raise InvalidImage # raises exception if they don't
+
+        for h, row in enumerate(new_img[PIXELS]): # boxed-row flat-pixel list
+            for i, value in enumerate(row):
+                self.image[h][i] += value*iterations # weight pixel values by iterations
+                          
+        self.iterations += iterations # increment instances iteration count
         
     def Write(self,filename):
-        # should work, even thoug Aptana complains
-        self.writer.write(filename, [[[channel / self.iterations for channel in pixel] for pixel in row] for row in self.image])
+        '''
+        Writes the image contained in this instance to file "filename"
+        
+        
+        @param filename: the file image will be written into
+        @return: nothing
+        
+        ''' 
+        print "Writing image to file", filename
+        
+        iterations = self.iterations if self.iterations > 0 else 1 #prevent division by zero
+        
+        # values are weighted sums, so divide by iteration count
+        scaled = [[value / iterations for value in row] for row in self.image] 
+        
+        #write to file (wb = write binary)
+        with open(filename, 'wb') as fp:
+            self.writer.write(fp, scaled)
+        print "Image writing completed!"
         
         
