9efa0c2cb38fcbac438a7c6455fe2878cd3fc9fe
Mikko Perttunen <cyndis@kapsi.fi>
2012-03-20 13:58:26 +0200

    Add sphere intersection test

--- backend/src/sphere.cpp
+++ backend/src/sphere.cpp
@@ -5,17 +5,40 @@
  */
 
 #include "sphere.h"
+#include <algorithm>
 
 dmcr::AABB dmcr::Sphere::aabb() const
 {
     return dmcr::AABB::
             fromCenterAndExtents(dmcr::SceneObject::position(),
-                                 dmcr::Vector3f(m_radius, m_radius, m_radius));
+                                 dmcr::Vector3f(m_radius*2, m_radius*2, m_radius*2));
 }
 
-dmcr::IntersectionResult dmcr::Sphere::intersects(const dmcr::Ray &/*ray*/)
+dmcr::IntersectionResult dmcr::Sphere::intersects(const dmcr::Ray & ray)
 const
 {
-    /* TODO: Implement me! */
+    dmcr::Vector3f ray_origin = ray.origin() - position();
     
+    float a = ray.direction().dot(ray.direction());
+    float b = 2 * ray.direction().dot(ray_origin);
+    float c = ray_origin.dot(ray_origin) - m_radius;
+
+    float dcrim = b * b - 4 * a * c;
+    if (dcrim < 0.0f) {
+        dmcr::IntersectionResult result;
+        result.intersects = false;
+        return result;
+    }
+
+    float a2inv = 1.0 / (2.0 * a);
+    float t0 = (-b + dcrim) * a2inv;
+    float t1 = (-b - dcrim) * a2inv;
+
+    dmcr::IntersectionResult result;
+    result.intersects = true;
+    result.t = std::min(t0, t1);
+    result.normal = (ray.origin() + result.t * ray.direction() - position()) / m_radius;
+    
+    return result;
 }
+
