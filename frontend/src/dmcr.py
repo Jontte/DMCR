b644b54a85848567fc8d85e5ce62f504b0a4794a
Arttu <arttu.voutilainen@gmail.com>
2012-03-21 11:37:03 +0200

    Add help text. use --help or -h

--- frontend/src/dmcr.py
+++ frontend/src/dmcr.py
@@ -9,6 +9,18 @@
 from server import Server
 import sys
 
+def help():
+    print """This is DMCR frontend version {}.
+For licensing see LICENSE.txt.
+
+Usage:
+    --width= / -w=: set width
+    --height= / -h=: set height
+    --file= / -f=: set file to render (json SDL)
+    --iterations= / -i=: set iterations
+    --help / -h: for this help
+"""
+
 def main():
     filename = "../../scenes/scene.json"
     width = 800
@@ -16,17 +28,20 @@ def main():
     iterations = 5
     if len(sys.argv) > 1: #if we have a filename
         for opt in sys.argv[1:]:
-            cmd, opt = opt.split("=")
-            if cmd == "--file" or cmd == '-f':
-                filename = opt
-            elif cmd == '--width' or cmd == '-w':
-                width = int(opt)
-            elif cmd == '--height' or cmd=='-h':
-                height = int(opt)
-            elif cmd == '--iterations' or cmd=='-i':
-                iterations = int(opt)
-            else:
-                print "Unknown option ({} = {}), use -f, -w, -h, -i.".format(cmd, opt)
+            if len(opt.split("=")) > 1:
+                cmd, opt = opt.split("=")
+                if cmd == "--file" or cmd == '-f':
+                    filename = opt
+                elif cmd == '--width' or cmd == '-w':
+                    width = int(opt)
+                elif cmd == '--height' or cmd=='-h':
+                    height = int(opt)
+                elif cmd == '--iterations' or cmd=='-i':
+                    iterations = int(opt)
+                else:  
+                    print "Unknown option ({} = {}), use -f, -w, -h, -i.".format(cmd, opt)
+             else:
+                 help()
 
 
     print "Starting FE server with:\n{}: {}x{}, {} iterations".format(filename, width, height, iterations)
