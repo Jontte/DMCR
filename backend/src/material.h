e6952aab64301ec9acde9418b65af9085cc95b47
Erra1 <hannu.siikonen@gmail.com>
2012-04-21 15:41:39 +0300

    edited material.h a bit more

--- backend/src/material.h
+++ backend/src/material.h
@@ -14,19 +14,32 @@ public:
     // Return color of material
     dmcr::Color color() const;
     
+
+    void setDiffuse(double diffuse){ m_diffuse = diffuse; }
+    void setSpecular(double specular){ m_specular = specular; }
+    void setTransmit(double transmit){ m_transmit = transmit; }
+    void setEmit(double emit){ m_emit = emit; }
     /*
         These functions return the probability of an arriving ray converting
         into a ray of given type.
         
         diffuse + specular + transmit + emit < 1
      */
-    double diffuse() const;
-    double specular() const;
-    double transmit() const;
-    double emit() const;
+
+    double diffuse() const{ return m_diffuse; }
+    double specular() const{ return m_specular; }
+    double transmit() const{ return m_transmit; }
+    double emit() const{ return m_emit; }
     
-    // Return refractive index of material
+    // Set and return refractive index of material
+    void setIor(double ior){ m_ior = ior};
     double ior() const;
+private:
+    double m_diffuse;
+    double m_specular;
+    double m_transmit;
+    double m_emit;
+    double m_ior;
 };
 
 }
