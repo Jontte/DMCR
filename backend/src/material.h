3cdc822b914f1fa1b2d5a7874c885115a6226aab
Mikko Perttunen <cyndis@kapsi.fi>
2012-04-21 17:17:23 +0300

    Fixes in material.h

--- backend/src/material.h
+++ backend/src/material.h
@@ -17,10 +17,10 @@ public:
     Material() : m_diffuse(), m_specular(), m_transmit(), m_emit(), m_ior() { }
 
     // Sets values for the reflection-parameters
-    void setDiffuse(double diffuse){ m_diffuse = diffuse; }
-    void setSpecular(double specular){ m_specular = specular; }
-    void setTransmit(double transmit){ m_transmit = transmit; }
-    void setEmit(double emit){ m_emit = emit; }
+    void setDiffuse(double diffuse) { m_diffuse = diffuse; }
+    void setSpecular(double specular) { m_specular = specular; }
+    void setTransmit(double transmit) { m_transmit = transmit; }
+    void setEmit(double emit) { m_emit = emit; }
     /*
         These functions return the probability of an arriving ray converting
         into a ray of given type.
@@ -28,14 +28,14 @@ public:
         diffuse + specular + transmit + emit < 1
      */
 
-    double diffuse() const{ return m_diffuse; }
-    double specular() const{ return m_specular; }
-    double transmit() const{ return m_transmit; }
-    double emit() const{ return m_emit; }
+    double diffuse() const { return m_diffuse; }
+    double specular() const { return m_specular; }
+    double transmit() const { return m_transmit; }
+    double emit() const { return m_emit; }
     
     // Set and return refractive index of material
-    void setIor(double ior){ m_ior = ior};
-    double ior() const { return m_ior; };
+    void setIor(double ior) { m_ior = ior; }
+    double ior() const { return m_ior; }
 
     // Set and return texture of material
     void setTexture(dmcr::Texture2DPtr texture) { m_texture = texture; }
