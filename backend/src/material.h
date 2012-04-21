33b7b93998c769b7d051ecaee7cc108f49065683
Jaakko Ketola <jaakko.ketola@aalto.fi>
2012-04-21 15:00:26 +0300

    Added prototypes for Material class' functions

--- backend/src/material.h
+++ backend/src/material.h
@@ -1,9 +1,34 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include"vector.h"
+
 //sets up diffuse, reflection, emit, transmit and absorption for a certain surface
 //(absorption = 1 - reflection - transmit - emit)
 
+namespace DMCR {
+    
+class Material
+{
+public:
+    // Return color of material
+    dmcr::Color color() const;
+    
+    /*
+        These functions return the probability of an arriving ray converting
+        into a ray of given type.
+        
+        diffuse + specular + transmit + emit < 1
+     */
+    double diffuse() const;
+    double specular() const;
+    double transmit() const;
+    double emit() const;
+    
+    // Return refractive index of material
+    double ior() const;
+};
 
+}
 
 #endif // MATERIAL_H
